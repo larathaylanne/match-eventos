57338bc16455612d4132784d3db1d75004b6d2f2
Nicolasnat <nicolas.natario0@gmail.com>
2026-04-05 20:01:13 -0300

    Adicionado o Login

--- backend/demo/src/main/java/com/example/demo/service/UserService.java
+++ backend/demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,21 @@ import com.example.demo.repository.UserRepository;
 public class UserService {
 
     @Autowired
-    private UserRepository usuarioRepository;
+    private UserRepository userRepository;
 
     public UserModel cadastrarUsuario(UserModel usuario) {
-        // Regra de negócio simples: salvar o usuário no banco
-        return usuarioRepository.save(usuario);
+        return userRepository.save(usuario);
+    }
+
+    public UserModel realizarLogin(String email, String senha) {
+        Optional<UserModel> usuarioOpt = userRepository.findByEmail(email);
+
+        if (usuarioOpt.isPresent()) {
+            UserModel usuario = usuarioOpt.get();
+            if (usuario.getSenha().equals(senha)) {
+                return usuario;
+            }
+       }
+       return null;
     }
 }
